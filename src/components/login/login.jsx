6917ea045e8300834970daace2df5fc9f8791961
ogiogiovictor@gmail.com <ogiogiovictor@gmail.com>
2022-07-09 11:51:19 -0600

    completed sign in and singup

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input';
 import CustomButton from '../custom-button/custom-button';
 
-import { signInWithGoogle } from '../../firebase/firebase.util';
+import { auth, signInWithGoogle } from '../../firebase/firebase.util';
 
 import './login.scss';
 
@@ -17,10 +17,16 @@ class Login extends React.Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-
-        this.setState({email: '', password: ''})
+        const { email, password} = this.state;
+        try{
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({email: '', password: ''})
+        }catch(e){
+            console.log(e)
+        }
+       
     }
 
     handleChange = event => {
